c2ff50028564ee783fb9c5f7d7dc6c9b14fc461a
Kchen833 <56746716+Kchen833@users.noreply.github.com>
2021-03-30 00:43:53 -0400

    Clean up 4
    This was never going to be used at the current rate.

--- errors.py
+++
@@ -1,7 +0,0 @@
-"""This file handles all of the custom errors from other files"""
-
-
-
-class InvalidFile(Error):
-    """Raised when the file is not a valid format(eg there are too few lines)"""
-    pass
